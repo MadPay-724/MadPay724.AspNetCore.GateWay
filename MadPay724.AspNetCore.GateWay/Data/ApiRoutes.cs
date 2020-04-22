f8c66839121a09cf439e4adadce75934d6a16d19
key.one72@gmail.com <key.one72@gmail.com>
2020-04-22 20:23:21 +0430

    ver 1.1.2

---
+++ MadPay724.AspNetCore.GateWay/Data/ApiRoutes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MadPay724.AspNetCore.GateWay.Data
+{
+    public static class ApiRoutes
+    {
+        public const string BaseUrl = "https://api.madpay724.ir";
+        public const string BaseApi = BaseUrl + "/v1/pg";
+
+        public static class Pay
+        {
+            //Post
+            public const string PaySend = BaseApi + "/pay";
+        }
+        public static class Verify
+        {
+            //Post
+            public const string VerifySend = BaseApi + "/verify";
+        }
+
+        public static class Refund
+        {
+            //Post
+            public const string RefundSend = BaseApi + "/refund";
+        }
+    }
+}
---
+++ MadPay724.AspNetCore.GateWay/MadPayGateWay.cs
@@ -0,0 +1,181 @@
+﻿using MadPay724.AspNetCore.GateWay.Data;
+using MadPay724.AspNetCore.GateWay.Models;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MadPay724.AspNetCore.GateWay
+{
+    public class MadPayGateWay : IMadPayGateWay, IDisposable
+    {
+        private readonly HttpClient _http;
+        private StringContent _content;
+        private HttpResponseMessage _response;
+
+        public MadPayGateWay()
+        {
+            _http = new HttpClient();
+        }
+
+        #region AsyncMethods
+
+        /// <summary>
+        /// Use this method for requesting a bank gate
+        /// </summary>
+        /// <param name="madPayGatePayRequest"></param>
+        /// <returns></returns>
+        public async Task<MadPayGateResult<MadPayGatePayResponse>> PayAsync(MadPayGatePayRequest madPayGatePayRequest)
+        {
+            _http.DefaultRequestHeaders.Clear();
+
+            _content = new StringContent(
+                JsonConvert.SerializeObject(madPayGatePayRequest), UTF8Encoding.UTF8, "application/json");
+
+            _response = await _http.PostAsync(ApiRoutes.Pay.PaySend, _content);
+
+            if ((int)_response.StatusCode == 200)
+            {
+                return JsonConvert
+                .DeserializeObject<MadPayGateResult<MadPayGatePayResponse>>(await _response.Content.ReadAsStringAsync());
+            }
+            else if((int)_response.StatusCode == 400)
+            {
+                var res = JsonConvert
+                .DeserializeObject<MadPayGateResult<string>>(await _response.Content.ReadAsStringAsync());
+
+                return new MadPayGateResult<MadPayGatePayResponse>
+                {
+                    Messages = res.Messages,
+                    Status = false,
+                    Result = null
+                };
+            }
+            else
+            {
+                return new MadPayGateResult<MadPayGatePayResponse>
+                {
+                    Messages = new string[] {"خطای نامشخص"},
+                    Status = false,
+                    Result = null
+                };
+            }
+        }
+        /// <summary>
+        /// Use this method for verifying the bank result
+        /// </summary>
+        /// <param name="madPayGateVerifyRequest"></param>
+        /// <returns></returns>
+        public async Task<MadPayGateResult<MadPayGateVerifyResponse>> VerifyAsync(MadPayGateVerifyRequest madPayGateVerifyRequest)
+        {
+            _http.DefaultRequestHeaders.Clear();
+
+            _content = new StringContent(
+                JsonConvert.SerializeObject(madPayGateVerifyRequest), UTF8Encoding.UTF8, "application/json");
+
+            _response = await _http.PostAsync(ApiRoutes.Verify.VerifySend, _content);
+
+            if ((int)_response.StatusCode == 200)
+            {
+                return JsonConvert
+                .DeserializeObject<MadPayGateResult<MadPayGateVerifyResponse>>(await _response.Content.ReadAsStringAsync());
+            }
+            else if ((int)_response.StatusCode == 400)
+            {
+                var res = JsonConvert
+                .DeserializeObject<MadPayGateResult<string>>(await _response.Content.ReadAsStringAsync());
+
+                return new MadPayGateResult<MadPayGateVerifyResponse>
+                {
+                    Messages = res.Messages,
+                    Status = false,
+                    Result = null
+                };
+            }
+            else
+            {
+                return new MadPayGateResult<MadPayGateVerifyResponse>
+                {
+                    Messages = new string[] { "خطای نامشخص" },
+                    Status = false,
+                    Result = null
+                };
+            }
+        }
+        /// <summary>
+        /// use this method for cancel the transaction
+        /// </summary>
+        /// <param name="madPayGateRefundRequest"></param>
+        /// <returns></returns>
+        public async Task<MadPayGateResult<MadPayGateRefundResponse>> RefundAsync(MadPayGateRefundRequest madPayGateRefundRequest)
+        {
+            _http.DefaultRequestHeaders.Clear();
+
+            _content = new StringContent(
+                JsonConvert.SerializeObject(madPayGateRefundRequest), UTF8Encoding.UTF8, "application/json");
+
+            _response = await _http.PostAsync(ApiRoutes.Refund.RefundSend, _content);
+
+            if ((int)_response.StatusCode == 200)
+            {
+                return JsonConvert
+                .DeserializeObject<MadPayGateResult<MadPayGateRefundResponse>>(await _response.Content.ReadAsStringAsync());
+            }
+            else if ((int)_response.StatusCode == 400)
+            {
+                var res = JsonConvert
+                .DeserializeObject<MadPayGateResult<string>>(await _response.Content.ReadAsStringAsync());
+
+                return new MadPayGateResult<MadPayGateRefundResponse>
+                {
+                    Messages = res.Messages,
+                    Status = false,
+                    Result = null
+                };
+            }
+            else
+            {
+                return new MadPayGateResult<MadPayGateRefundResponse>
+                {
+                    Messages = new string[] { "خطای نامشخص" },
+                    Status = false,
+                    Result = null
+                };
+            }
+        }
+
+        #endregion
+
+        #region Dispose
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    _http.Dispose();
+                    if(_content != null)
+                        _content.Dispose();
+                    if (_response != null)
+                        _response.Dispose();
+                    //
+                    disposed = true;
+                }
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~MadPayGateWay()
+        {
+            Dispose(true);
+        }
+        #endregion
+
+    }
+}
